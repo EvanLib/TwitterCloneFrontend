423d0b229d0012942d5076592f5d324eb46b7c4a
Evan <evangrayson95@gmail.com>
2017-04-16 15:57:42 -0400

    Logout Function: fix maybe?

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -9,7 +9,7 @@ import { AuthHttp, tokenNotExpired } from 'angular2-jwt';
 
 export class AuthService {
   public token: string;
-  
+
   constructor(private authHttp: AuthHttp, private http: Http) {
         // set token if saved in local storage
         var currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -20,13 +20,13 @@ export class AuthService {
   }
 
   logout(): Observable<boolean> {
-    //Delete local storage
-    localStorage.removeItem('id_token')
-
     //Tell API to remove User Token
-    this.authHttp.get('http://localhost:3000/api/auth/logout')
+    this.authHttp.delete('http://localhost:3000/api/auth/logout')
     .subscribe((res:Response) => {
-      if (res.ok) {
+      console.log("Stuff")
+      if (res.ok) { //Returns a status of okay http 20*
+        //Delete local storage token
+        localStorage.removeItem('id_token')
         return Observable.of(true);
       }
       return Observable.of(false);
@@ -50,8 +50,8 @@ export class AuthService {
                     this.token = token;
 
                     // store username and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('id_token', token)
-
+                    localStorage.setItem('id_token', this.token)
+                    console.log(localStorage.getItem('id_token'))
                     // return true to indicate successful login
                     return true;
                 } else {
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
 
+
 function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp(new AuthConfig({
     tokenName: 'id_token',
